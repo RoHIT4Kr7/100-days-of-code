681b342ed28ac1fb174debcabd581ea202c96661
Rohit Kumar <146173106+RoHIT4Kr7@users.noreply.github.com>
2025-07-16 00:31:49 +0530

    check commit issue

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator.")
 first_name = input("What's name of the city you grew up in?\n")
 second_name = input("What's your pet's name?\n")
-print("Your band name could be " + first_name + " " + second_name)
+print("Your band name could be " + first_name + " " + second_name+"its a check for commit")
